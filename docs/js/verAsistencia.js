53e3118c7a0dbe34fd745f3e5a69c40fb8fa7076
Blady-87 <cmv2018174@est.univalle.edu>
2025-04-24 13:16:42 -0400

    Agregado formulario

---
+++ docs/js/verAsistencia.js
@@ -0,0 +1,34 @@
+function verificarInvitada(event) {
+    event.preventDefault();
+  
+    const frase = document.querySelectorAll('.input-field')[0];
+    const codigo = document.querySelectorAll('.input-field')[1];
+  
+    const fraseValida = frase.value.trim().toLowerCase() === "vane";
+    const codigoValido = codigo.value.trim() === "holaver12";
+  
+    if (fraseValida && codigoValido) {
+      window.open("https://maps.app.goo.gl/ZKmrpK1D7zo7XAKw9", "_blank");
+    } else {
+      if (!fraseValida) {
+        frase.value = "";
+        frase.placeholder = "Frase incorrecta";
+        frase.style.border = "2px solid red";
+      }
+  
+      if (!codigoValido) {
+        codigo.value = "";
+        codigo.placeholder = "Código incorrecto";
+        codigo.style.border = "2px solid red";
+      }
+    }
+  }
+  
+  // Limpia el borde rojo cuando se empieza a escribir de nuevo
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.input-field').forEach(input => {
+      input.addEventListener('input', () => {
+        input.style.border = "";
+      });
+    });
+  });
